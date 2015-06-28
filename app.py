319f2ffd9e6735e393efd41068a02d90805452c7
Yuusuke KOUNOIKE <kounoike.yuusuke@gmail.com>
2015-06-28 22:09:07 +0900

    色々

--- app.py
+++ app.py
@@ -1,17 +1,20 @@
 #!python
 # coding: utf-8
 
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 import ldap
+import ldap.modlist
 import hashlib
 import base64
 import os
 
 app = Flask(__name__)
 app.debug = True
+app.secret_key = "my secret key"
 
 ldap_host = "localhost"
-ldap_baseDN = "dc=typemiss,dc=net"
+ldap_baseDN = "ou=Person,dc=typemiss,dc=net"
+managerDN = "cn=Manager,dc=typemiss,dc=net"
 
 
 def make_secret(password):
@@ -21,12 +24,10 @@ def make_secret(password):
     return "{SSHA}" + base64.encodestring(h.digest() + salt)
 
 
-def error(message):
-    return render_template("error.html", message=message)
-
-
-def success():
-    return render_template("success.html")
+def openldap():
+    l = ldap.open(ldap_host)
+    l.protocol_version = ldap.VERSION3
+    return l
 
 
 @app.route("/", methods=["POST", "GET"])
@@ -36,30 +37,112 @@ def index():
         oldpassword = request.form.get("oldpassword")
         newpassword1 = request.form.get("newpassword1")
         newpassword2 = request.form.get("newpassword2")
+        dn = request.form.get("dn")
 
         if newpassword1 == "" or newpassword1 != newpassword2:
-            return error(u"パスワードが一致しません")
+            session["err"] = u"パスワードが一致しません"
+            return redirect(url_for("index"))
 
-        l = ldap.open(ldap_host)
-        l.protocol_version = ldap.VERSION3
-        searchScope = ldap.SCOPE_SUBTREE
-        retrieveAttributes = ["mail", "dn"]
-        result_list = l.search_s(ldap_baseDN, searchScope, "uid=%s" % username)
-        if len(result_list) == 0:
-            return error(u"ユーザ名が見つかりません")
-        dn = result_list[0][0]
-        print dn
+        l = openldap()
         try:
             l.simple_bind_s(dn, oldpassword)
         except ldap.INVALID_CREDENTIALS, e:
-            return error(u"旧パスワードが違います")
+            session["err"] = u"旧パスワードが違います"
+            return redirect(url_for("index"))
         secret = make_secret(newpassword1)
-        print secret
         l.modify_s(dn, [(ldap.MOD_REPLACE, "userPassword", secret)])
 
-        return success()
+        session["success"] = u"変更しました"
+        return redirect(url_for("index"))
     else:
-        return render_template("index.html")
+        l = openldap()
+        searchScope = ldap.SCOPE_SUBTREE
+        result_list = l.search_s(ldap_baseDN, searchScope, "objectClass=inetOrgPerson")
+        err = session.get("err")
+        success = session.get("success")
+        session["err"] = None
+        session["success"] = None
+        return render_template("index.html", **locals())
+
+
+@app.route("/admin", methods=["GET", "POST"])
+def admin():
+    if request.method == "POST":
+        newpassword1 = request.form.get("newpassword1")
+        newpassword2 = request.form.get("newpassword2")
+        dn = request.form.get("dn")
+        if newpassword1 == "" or newpassword1 != newpassword2:
+            session["err"] = u"パスワードが一致しません"
+            return redirect(url_for("admin"))
+        l = openldap()
+        try:
+            l.simple_bind_s(managerDN, request.form.get("adminpassword"))
+        except ldap.INVALID_CREDENTIALS, e:
+            session["err"] = u"管理者パスワードが違います"
+            return redirect(url_for("admin"))
+        secret = make_secret(newpassword1)
+        l.modify_s(dn, [(ldap.MOD_REPLACE, "userPassword", secret)])
+
+        session["success"] = u"成功しました"
+        return redirect(url_for("admin"))
+    else:
+        l = openldap()
+        searchScope = ldap.SCOPE_SUBTREE
+        result_list = l.search_s(ldap_baseDN, searchScope, "objectClass=inetOrgPerson")
+        success = session.get("success")
+        err = session.get("err")
+        session["err"] = None
+        session["success"] = None
+        return render_template("admin.html", **locals())
+
+
+@app.route("/admin_adduser", methods=["GET", "POST"])
+def admin_adduser():
+    if request.method == "POST":
+        newpassword1 = request.form.get("newpassword1")
+        newpassword2 = request.form.get("newpassword2")
+        uid = request.form.get("username")
+        sn = request.form.get("sn")
+        givenName = request.form.get("givenName")
+        displayName = "{0} {1}".format(sn, givenName)
+        mail = request.form.get("email")
+        dn = "uid={0},{1}".format(uid, ldap_baseDN)
+        if newpassword1 == "" or newpassword1 != newpassword2:
+            session["err"] = u"パスワードが一致しません"
+            return redirect(url_for("admin_adduser"))
+        l = openldap()
+        try:
+            l.simple_bind_s(managerDN, request.form.get("adminpassword"))
+        except ldap.INVALID_CREDENTIALS, e:
+            session["err"] = u"管理者パスワードが違います"
+            return redirect(url_for("admin_adduser"))
+        secret = make_secret(newpassword1)
+        attrs = {
+            "uid": str(uid),
+            "objectClass": "top",
+            "objectClass": "inetOrgPerson",
+            "cn": str(uid),
+            "sn": str(sn),
+            "givenName": str(givenName),
+            "displayName": str(displayName),
+            "mail": str(mail),
+            "userPassword": secret
+        }
+        try:
+            l.add_s(dn, ldap.modlist.addModlist(attrs))
+        except ldap.LDAPError, e:
+            session["err"] = u"LDAPエラーが発生しました:{0}".format(e)
+            return redirect(url_for("admin_adduser"))
+
+        session["success"] = u"成功しました"
+        return redirect(url_for("admin_adduser"))
+    else:
+        success = session.get("success")
+        err = session.get("err")
+        session["err"] = None
+        session["success"] = None
+        return render_template("admin_adduser.html", **locals())
+
 
 if __name__ == "__main__":
     port = 5100
--- ldappw.cgi
+++ ldappw.cgi
@@ -2,5 +2,12 @@
 
 from wsgiref.handlers import CGIHandler
 from app import app
+import mimetypes
+
+mimetypes.add_type("application/x-font-woff", ".woff")
+mimetypes.add_type("application/font-woff2", ".woff2")
+mimetypes.add_type("application/x-font-otf", ".otf")
+mimetypes.add_type("application/octet-stream", ".ttf")
+mimetypes.add_type("application/vnd.ms-fontobject", ".eot")
 
 CGIHandler().run(app)
